eeaa61711f08606dd29d0ba2da7b0652b611812a
Edwin Zhou <edwinzzhou@gmail.com>
2015-10-10 13:42:24 -0400

    Add gulpfile and boilerplate code

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/* global PIXI */
+'use strict';
+
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+renderer.render(stage);
