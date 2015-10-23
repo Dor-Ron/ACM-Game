640c168c79df827d7b052e385a3cd5c745657e61
Dor-Ron <dor.rondel@gmail.com>
2015-10-23 15:20:03 -0400

    Added title, and test for paddle collision detection

--- js/index.js
+++ js/index.js
@@ -9,7 +9,8 @@
 var Container = PIXI.Container,
     Graphics = PIXI.Graphics,
     autoDetectRenderer = PIXI.autoDetectRenderer,
-    loader = PIXI.loader;  //might need this one later    
+    loader = PIXI.loader; //might need this one later   
+    Text =  PIXI.Text   
 
 //                              width, height
 var renderer = autoDetectRenderer(650, 700); //we'll play with this later, in original height > width as oppose to most games...
@@ -22,6 +23,18 @@ var thirdOfWidth = (renderer.view.width / 3) - 10; //A little less than 3rd of w
 /////////////////////
 /////Canvas Setup////
 /////////////////////
+
+//Text for game
+var gameTitle = new Text(
+    "Pel 2.0",
+  {font: "32px PetMe64", fill: "white"}
+);
+
+gameTitle.position.set(renderer.view.width/2 - 45, 20); // Suppose to center title, we'll work on it later, 
+stage.addChild(gameTitle);                              // align: "center" doesnt work on single line text, 
+                                                        // Need a way to center it on all screen sizes....
+
+
 
 //Initial game appearence  wit
 var paddle = new Graphics();
@@ -70,6 +83,30 @@ var paddle2 = {
 		renderer.render(stage);		
 	}
 };
+
+
+/////////////////////
+//////// Balls //////
+/////////////////////
+
+
+//Two circles for collision detection demo
+var circle = new Graphics();
+circle.beginFill(0xccf205);
+circle.drawCircle(0, 0, 14);
+circle.endFill();
+circle.x = 64;
+circle.y = 130;
+stage.addChild(circle);
+
+
+var circle2 = new Graphics();
+circle2.beginFill(0xfdbf00);
+circle2.drawCircle(0, 0, 14);
+circle2.endFill();
+circle2.x = thirdOfWidth * 3;
+circle2.y = renderer.view.height - 30;
+stage.addChild(circle2);
 
 
 /////////////////////
@@ -128,6 +165,7 @@ left.press = function() {
 left.release = function() {
   	//key object released
   	paddle2.drawPaddle();  //draw left move
+    console.log(hitPaddleTest(paddle, circle2)) //erase later just for test
 };
 
 right.press = function() {
@@ -139,6 +177,63 @@ right.press = function() {
 right.release = function() {
 	//key object released
 	paddle2.drawPaddle();  //draw right move
+  console.log(hitPaddleTest(paddle, circle2)) //erase later, just for test
+};
+
+
+///////////////////////
+//Collision Detection//
+///////////////////////
+
+function hitPaddleTest(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+
+    //A collision might be occuring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+
+      //There's no collision on the y axis
+      hit = false;
+    }
+  } else {
+
+    //There's no collision on the x axis
+    hit = false;
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit;
 };
 
 
